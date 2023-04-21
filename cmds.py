f7010e3da0e222c790be8a6728ce87d16ce7b7ad
chenjunqian <chenjunqian0810@foxmail.com>
2023-04-22 01:10:37 +0800

    feat: add web deploy script

---
+++ cmds.py
@@ -0,0 +1,39 @@
+crawler_stop_pull_restart_cmd = '''
+echo "stop guoshaofm-crawler"
+docker stop guoshaofm-crawler
+echo "remove guoshaofm-crawler container"
+docker container rm guoshaofm-crawler
+echo "remove guoshaofm-crawler image"
+docker rmi guoshaofm-crawler
+echo "pull guoshaofm-crawler image"
+docker pull beegedelow/guoshaofm-crawler
+
+LOGS_DIR=/home/guoshaofm-crawler/logs
+if [[ ! -e $LOGS_DIR ]]; then
+    mkdir -p $LOGS_DIR
+elif [[ ! -d $LOGS_DIR ]]; then
+    echo "$LOGS_DIR already exists but is not a directory" 1>&2
+fi
+echo "run guoshaofm-crawler container"
+docker run --name guoshaofm-crawler --network host --log-opt max-size=500m --log-opt max-file=3 -v $LOGS_DIR:/app/logs -d beegedelow/guoshaofm-crawler
+'''
+
+web_stop_pull_restart_cmd = '''
+echo "stop guoshaofm-web"
+docker stop guoshaofm-web
+echo "remove guoshaofm-web container"
+docker container rm guoshaofm-web
+echo "remove guoshaofm-web image"
+docker rmi guoshaofm-web
+echo "pull guoshaofm-web image"
+docker pull beegedelow/guoshaofm-web
+
+LOGS_DIR=/home/guoshaofm-web/logs
+if [[ ! -e $LOGS_DIR ]]; then
+    mkdir -p $LOGS_DIR
+elif [[ ! -d $LOGS_DIR ]]; then
+    echo "$LOGS_DIR already exists but is not a directory" 1>&2
+fi
+echo "run guoshaofm-web container"
+docker run --name guoshaofm-web --network host --log-opt max-size=500m --log-opt max-file=3 -v $LOGS_DIR:/app/logs -d beegedelow/guoshaofm-web
+'''
--- deploy_web.py
+++ deploy_web.py
@@ -1,8 +1,8 @@
 from fabric import Connection
-from consts import crawler_hosts
-from cmds import crawler_stop_pull_restart_cmd
+from consts import web_hosts
+from cmds import web_stop_pull_restart_cmd
 
 if __name__ == "__main__":
-    for host in crawler_hosts:
+    for host in web_hosts:
         with Connection(host=host, user="root", connect_kwargs={'password': "1qaz!QAZ70233374"}) as conn:
-            conn.run(crawler_stop_pull_restart_cmd)
+            conn.run(web_stop_pull_restart_cmd)

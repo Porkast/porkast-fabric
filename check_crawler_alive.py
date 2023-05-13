8fd550ca94e820a7787f68e35bbe31c3738f1ab1
chenjunqian <chenjunqian0810@foxmail.com>
2023-05-13 22:10:44 +0800

    feat: add host info list

--- check_crawler_alive.py
+++ check_crawler_alive.py
@@ -1,9 +1,9 @@
 from fabric import Connection
-from consts import crawler_hosts
+from consts import crawler_host_info_list
 from cmds import check_crawler_alive_cmd
 
 if __name__ == "__main__":
-    for host in crawler_hosts:
-        with Connection(host=host, user="root", connect_kwargs={'password': "1qaz!QAZ70233374"}) as conn:
-            current_cmd = check_crawler_alive_cmd.format(host = host)
+    for host_info in crawler_host_info_list:
+        with Connection(host=host_info["host"], user="root", connect_kwargs={'password': "1qaz!QAZ70233374"}) as conn:
+            current_cmd = check_crawler_alive_cmd.format(host = host_info["name"])
             conn.run(current_cmd)
--- consts.py
+++ consts.py
@@ -1,3 +1,17 @@
 crawler_hosts = ["8.218.181.215", "8.217.22.1", "8.217.21.24"]
+crawler_host_info_list = [
+    {
+        "name": "guoshao-crawler-001",
+        "host": "8.218.181.215"
+    },
+    {
+        "name": "guoshao-crawler-002",
+        "host": "8.217.22.1"
+    },
+    {
+        "name": "guoshao-crawler-003",
+        "host": "8.217.21.24"
+    }
+]
 
 web_hosts = ["47.242.232.137"]

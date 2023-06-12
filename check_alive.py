cb0e6d599ccc81e69c523a9decb77ccf13ac0ddb
chenjunqian <chenjunqian0810@foxmail.com>
2023-06-12 22:30:17 +0800

    fix: catch exception for single request

--- check_alive.py
+++ check_alive.py
@@ -5,9 +5,13 @@ from cmds import check_crawler_alive_cmd, check_elasricsearch_alive_cmd
 
 if __name__ == "__main__":
     for host_info in crawler_host_info_list:
-        with Connection(host=host_info["host"], user="root", connect_kwargs={'password': "1qaz!QAZ70233374"}) as conn:
-            current_cmd = check_crawler_alive_cmd.format(host = host_info["name"])
-            conn.run(current_cmd)
+        try:
+            with Connection(host=host_info["host"], user="root", connect_kwargs={'password': "1qaz!QAZ70233374"}) as conn:
+                current_cmd = check_crawler_alive_cmd.format(host = host_info["name"])
+                conn.run(current_cmd)
+        except Exception as e:
+            print("connect ", host_info["name"], " failed : ", e)
+        
 
     for host_info in elasticsearch_hosts:
         with Connection(host=host_info["host"], user="root", connect_kwargs={'password': "1qaz!QAZ70233374"}) as conn:
